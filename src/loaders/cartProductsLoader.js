363184056b911f26ddc24ee7a624b775083c678f
SyedHasibAhmed <syedhasibahmed11@gmail.com>
2024-01-23 01:53:11 +0600

    remove from DB

---
+++ src/loaders/cartProductsLoader.js
@@ -0,0 +1,32 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
+const cartProductsLoader = async () =>{
+    const loaderProducts = await fetch('products.json');
+    const products = await loaderProducts.json();
+
+    // if Cart data is in database, you have to use async await
+    const storedCart = getShoppingCart();
+
+    const savedCart = [];
+
+    console.log(storedCart);
+    for(const id in storedCart){
+        const addedProduct = products.find(pd => pd.id === id);
+        if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+
+    // if you need to send two things
+    // return [products, savedCart]
+
+    // another options
+    // return { products, cart: savedCart}
+
+    // console.log(products);
+    return savedCart;
+}
+
+export default cartProductsLoader;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './components/Shop/Shop.jsx';
+import Home from './components/Layout/Home.jsx';
+import Orders from './components/Orders/Orders.jsx';
+import Inventory from './components/Inventory/Inventory.jsx';
+import Login from './components/Login/Login.jsx';
+import cartProductsLoader from './loaders/cartProductsLoader.js';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Home></Home>,
+    children:[
+      {
+        path:'/',
+        element:<Shop></Shop>
+      },
+      {
+        path:'/orders',
+        element:<Orders></Orders>,
+        loader: cartProductsLoader
+      },
+      {
+        path:'/inventory',
+        element:<Inventory></Inventory>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
